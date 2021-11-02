d51e913005cf9021b73760d9d3252466581d2a8d
zunggzing <hello.trungvinh@gmail.com>
2021-11-02 23:29:35 +0700

    Add apigetway

---
+++ src/main/java/com/apigetway/ApigetwayApplication.java
@@ -0,0 +1,15 @@
+package com.apigetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApigetwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigetwayApplication.class, args);
+	}
+
+}
